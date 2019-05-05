dc8cde485c68a90d7ad1cf0200cfba487f6a954b
mateusz-obszanski <50120373+mateusz-obszanski@users.noreply.github.com>
2019-05-05 19:43:46 +0200

    Add files via upload

--- snake.hpp
+++ snake.hpp
@@ -2,6 +2,7 @@
 #define
 
 #include <utility>
+#include <vector>
 
 namespace Snake{
 
@@ -10,23 +11,61 @@ namespace Snake{
     };
 
     typedef std::pair<std::size_t, std::size_t> Coordinates2D;
-    using Coordinates2D.first = x;
-    using Coordinates2D.second = y;
+
+    class Bend{
+    private:
+        Coordinates2D coordinates_;
+    public:
+        Bend(Coordinates2D coordinates) : coordinates_(coordinates){}
+    };
+
+    typedef vector<Bend> Bends;
 
     class Snake{
     private:
-        Coordinates2D headCoordinates;
+        std::size_t minLength;
+        Coordinates2D headCoordinates_;
+        Coordinates2D tailCoordinates_;
+        DIRECTION headOrientation_;
+        DIRECTION tailOrientation;
+        Bends bends_;
     public:
         void move(DIRECTION direction){
             switch (direction) {
                 case DIRECTION::UP:
-                    ++y;
+                    ++headCoordinates_.second;
+                    break;
                 case DIRECTION:DOWN:
-                    --y;
+                    --headCoordinates_.second;
+                    break;
                 case DIRECTION:LEFT:
-                    --x;
+                    --headCoordinates_.first;
+                    break;
                 case DIRECTION::RIGHT:
-                    ++x;
+                    ++headCoordinates_.first;
+                    break;
+            }
+            if(headOrientation_ != direction) {
+                bends_.push_back(Bend(headCoordinates_));
+            }
+            for(std::size_t i = bends_.begin(); i < bends_.end(); ++i){
+                if(i.coordinates_ == tailCoordinates_){
+                    bends_.erase(i);
+                }
+            }
+            switch(tailOrientation){
+                case DIRECTION::UP:
+                    ++tailCoordinates_.second;
+                    break;
+                case DIRECTION::DOWN:
+                    --tailCoordinates_.second;
+                    break;
+                case DIRECTION::LEFT:
+                    --tailCoordinates_.first;
+                    break;
+                case DIRECTION::RIGHT:
+                    ++tailCoordinates_.first;
+                    break;
             }
         }
     };
