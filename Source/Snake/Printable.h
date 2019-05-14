c5512a50a7d5aceceb988999da7170f5bbc2adb5
michunie <michunie@gmail.com>
2019-05-14 10:46:54 +0200

    Dodano kalsę board oraz snake_body, nie wiem jaki znak ma drukować sanke'a oraz w jakim formacie jest zapisywany score board

---
+++ Source/Snake/Printable.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<iostream>
+
+class printable {
+public:
+	virtual void print() = 0;
+	virtual ~ printable () {}
+};
+
+class fruit : public printable {
+public: 
+	void print() override { //przyjnuje jak¹ zmienna i w ze¿noœci od tego mo¿e rysowaæ rózne owocki
+		std::cout << "$";
+	}
+
+};
+
+class board :public printable {
+public:
+	void print() override {
+		print_score();
+		print_timer();
+	}
+	void print_score()
+	void print_timer()
+};
+
+class snake_body : public printable {
+public:
+	void print() override {
+		std::cout << ""; //nie wiem co ma byæ cia³em sanke'a
+	}
+};
+
+
+print_game();
