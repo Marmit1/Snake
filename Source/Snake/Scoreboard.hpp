e414b5261a5d3ce516bfce6e7b238adeaca797db
Marcin Mitura <50170317+Marmit1@users.noreply.github.com>
2019-05-20 23:30:18 +0200

    usunięty timer

--- Source/Snake/Scoreboard.hpp
+++ Source/Snake/Scoreboard.hpp
@@ -12,24 +12,15 @@
 class Timer {
 public:
 
-	Timer(std::size_t sec = 0, std::size_t min = 0, bool Swich = true) : sec_(sec), min_(min), Swich_(Swich) { timerSwich(); }
-
-	void Stop() { Swich_ = 0; }
-	void Start() { Swich_ = 1; }
-
+	
 private:
-	std::size_t sec_;
-	std::size_t min_; 
-	bool Swich_;
-
-	void timerSwich();
 	friend std::ostream &operator<< (std::ostream&, const Timer&);
 };
 
 
 class Scoreboard {
 public:
-	Scoreboard(std::size_t score = 0, Timer time = 0) : score_(score), time_(time) {}
+	Scoreboard(std::size_t score = 0) : score_(score), time_(time) {}
 
 	void resetTimer();
 
