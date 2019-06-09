ae5f22dc71cecfdcb15619b268b54dcf3c40177b
michunie <50120638+michunie@users.noreply.github.com>
2019-06-09 21:41:04 +0200

    Usuwanie niepotrzebych komentarzy

--- main.cpp
+++ main.cpp
@@ -1,50 +1,50 @@
-#include <iostream>										 // Używa cout/cin
-#include <conio.h>										 // Używa funkcje getch()
-#include <windows.h>					 				 // Używa funkcji windows
-#include <ctime>						 				 // Używa funkcji czas (prędkość gry)
-#include <vector>										 // Używa funkcji Pozycje
-#include <fstream>									     // Używa funkcji do wysokiego wyniku
+#include <iostream>										 
+#include <conio.h>										 
+#include <windows.h>					 				 
+#include <ctime>						 				
+#include <vector>										
+#include <fstream>									     
 #include <string>
 
 
 
 using namespace std;
-#define WIDTH 20										// Długość Plańszy
-#define HEIGHT 20										// Szerokość Plańszy
+#define WIDTH 20									
+#define HEIGHT 20									
 char Level[HEIGHT][WIDTH];
 
 
 // Deklaracja funkcji
-void Initialize(int rozmiar, int snakeX[], int snakeY[]);														// Inicjacja Węża
-void ClearScreen();																							// Jasność obrazu
-void BudowaPoziomu();																							// Budowa Poziomu
-void RysunekPlanszy();																								// Rysunek gry
-void Poruszanie(int &rozmiar, int snakeX[], int snakeY[], int tempX[], int tempY[], int &item_count, int &punkty); // Aktualizacja pętli gry
-void PredkoscPoruszania();																							// Aktualizacja Prędkości węża
-void Pozycje(vector<int> &itemX, vector<int> &itemY, int &item_count, int snakeX[], int snakeY[], int &rozmiar, int tailX, int tailY, int &punkty);	// Powiększanie i odżywianie się Węża
-bool IsGameOver(int snakeX[], int snakeY[], int rozmiar);														// Funkcja Koniec Gry
-void bialy();																								// Kolor nieaktywnych opcji
-void magnetowy();																							// Kolor aktywnej opcji
-void PodMenu();																								// Wybór Poziomu Trudności
-void Menu();																								// Menu główne
-void snake();																								// Rozpoczęcie Gry
-void Opcje();																								// Opcje
-void Aktywne();																								// Zmiana koloru opcji Aktywnych
-void Nieaktywne();																							// Zmiana koloru opcji Nieaktywnych
-void GameOver();																							// Funkcja Koniec Gry
-void Gwiazdka();																							// Gwiazdka po lewej stronie
-void Gwiazdka2();																							// Gwiazdka po prawej stronie
-void Sterowanie();																							// Wyświetla informacje o sterowaniu
+void Initialize(int rozmiar, int snakeX[], int snakeY[]);														
+void ClearScreen();																							
+void BudowaPoziomu();																							
+void RysunekPlanszy();																								
+void Poruszanie(int &rozmiar, int snakeX[], int snakeY[], int tempX[], int tempY[], int &item_count, int &punkty); 
+void PredkoscPoruszania();																							
+void Pozycje(vector<int> &itemX, vector<int> &itemY, int &item_count, int snakeX[], int snakeY[], int &rozmiar, int tailX, int tailY, int &punkty);	Węża
+bool IsGameOver(int snakeX[], int snakeY[], int rozmiar);														
+void bialy();																								
+void magnetowy();																							
+void PodMenu();																								
+void Menu();																								
+void snake();																								
+void Opcje();																								
+void Aktywne();																								
+void Nieaktywne();																							
+void GameOver();																							
+void Gwiazdka();																							
+void Gwiazdka2();																							
+void Sterowanie();																							
 
 
 // ZMIENNE
-int n = 1, z;											// Poruszanie się klawiaturą w PodMenu
-int m = 1;												// Poruszanie się klawiaturą w Menu
-int o = 1;												// Poruszanie się klawiaturą w Opcjach
-int p = 1;												// Poruszanie się klawiaturą w Zmianie koloru opcji Aktywnych
-int r = 1;												// Poruszanie się klawiaturą w Zmianie koloru opcji Nieaktywnych
-int x = 1;												// Poruszanie się klawiaturą w Funkcji GameOver
-int nieaktywny = 1;										// Zmienna pozwalające zmiane Koloru wyświetlania opcji Nieaktywnych
+int n = 1, z;											
+int m = 1;												
+int o = 1;												
+int p = 1;												
+int r = 1;											
+int x = 1;												
+int nieaktywny = 1;									
 int aktywny = 1;
 
 // Budowa Poziomu
@@ -104,9 +104,9 @@ void Poruszanie(int &rozmiar, int snakeX[], int snakeY[], int tempX[], int tempY
     bool gameOver = false;
     vector<int> itemX, itemY;
 
-    while (!gameOver)                                    // Pętla aż gra się skończy
+    while (!gameOver)                                    
     {
-        PredkoscPoruszania();                                    // Prędkość
+        PredkoscPoruszania();                                    
 
         // Dodanie ogona podczas poruszania
         tailY = snakeY[rozmiar - 1];
@@ -119,8 +119,8 @@ void Poruszanie(int &rozmiar, int snakeX[], int snakeY[], int tempX[], int tempY
         // Kopia węża (z wyjątkiem ogona) do tablicy temp
         // Zostawienie pierwszego elementy
         for (int i = 0; i < rozmiar - 1; i++) {
-            tempX[i + 1] = snakeX[i];                        // Kopiowanie wartości y
-            tempY[i + 1] = snakeY[i];                        // Kopiowanie wartości x
+            tempX[i + 1] = snakeX[i];                       
+            tempY[i + 1] = snakeY[i];                       
         }
 
         // Kopia głowy do pierwszego elementy aby mógł zostać przeniesiony przez użytkownika
@@ -140,34 +140,34 @@ void Poruszanie(int &rozmiar, int snakeX[], int snakeY[], int tempX[], int tempY
 
         // WHILE MOVING DOWN
         if (previnput == 's') {
-            snakeY[0] += 1;                                // Zestaw głów na nowej pozycji
-            if (input == 'a' || input == 'A')            // Idź w lewo
+            snakeY[0] += 1;                                
+            if (input == 'a' || input == 'A')            
                 previnput = 'a';
-            else if (input == 'd' || input == 'D')    // Idź w prawo
+            else if (input == 'd' || input == 'D')    
                 previnput = 'd';
         }
             // WHILE MOVING UP
         else if (previnput == 'w') {
-            snakeY[0] -= 1;                                // Zestaw głów na nowej pozycji
-            if (input == 'a' || input == 'A')        // Idź w lewo
+            snakeY[0] -= 1;                                
+            if (input == 'a' || input == 'A')       
                 previnput = 'a';
-            else if (input == 'd' || input == 'D')    // Idź w prawo
+            else if (input == 'd' || input == 'D')    
                 previnput = 'd';
         }
             // WHILE MOVING RIGHT
         else if (previnput == 'd') {
-            snakeX[0] += 1;                                // Zestaw głów na nowej pozycji
-            if (input == 'w' || input == 'W')        // Idź w górę
+            snakeX[0] += 1;                                
+            if (input == 'w' || input == 'W')        
                 previnput = 'w';
-            else if (input == 's' || input == 'S')    // Idź w dół
+            else if (input == 's' || input == 'S')   
                 previnput = 's';
         }
             // WHILE MOVING LEFT
         else if (previnput == 'a') {
             snakeX[0] -= 1;
-            if (input == 'w' || input == 'W')            // Idź w góre
+            if (input == 'w' || input == 'W')           
                 previnput = 'w';
-            else if (input == 's' || input == 'S')    // Idź w dół
+            else if (input == 's' || input == 'S')   
                 previnput = 's';
         }
     }
